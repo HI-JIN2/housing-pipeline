e670a69300ac081adf0d821a439c67841fbde25b
Yu Jin <qldls0307@naver.com>
2026-03-21 19:25:00 +0900

    fix: terraform backend configuration to use github secrets

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "http" {
     # Replace with your OCI Object Storage Pre-Authenticated Request (PAR) URL
-    # Example: https://objectstorage.ap-chuncheon-1.oraclecloud.com/p/.../b/terraform-state/o/terraform.tfstate
-    address        = "INSERT_YOUR_PAR_URL_HERE"
+    # This URL will be overridden in GitHub Actions using -backend-config
+    address        = "https://placeholder-overwrite-me"
     update_method  = "PUT"
   }
 }
