7fe1260fce5c7195776b9a9fac35295644a4c2aa
Yu Jin <qldls0307@naver.com>
2026-03-22 00:29:34 +0900

    fix: 로컬 환경 Kakao Map API 키 연동 (Vite)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load env from repo root
+  const env = loadEnv(mode, path.resolve(__dirname, '..'), '')
+  
+  // Set variable for index.html replacement
+  process.env.VITE_KAKAO_JS_KEY = env.KAKAO_JS_KEY || ''
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000',
+          changeOrigin: true,
+        }
       }
     }
   }
